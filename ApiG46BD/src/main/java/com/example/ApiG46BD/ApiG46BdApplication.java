1d888f9531ad023a76ac763bc99ef14f32d61678
JUANJO-P1 <jujogalo714@gmail.com>
2022-11-30 20:49:49 -0500

    commit

--- ApiG46BD/src/main/java/com/example/ApiG46BD/ApiG46BdApplication.java
+++ ApiG46BD/src/main/java/com/example/ApiG46BD/ApiG46BdApplication.java
@@ -2,7 +2,7 @@ package com.example.ApiG46BD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//prueba
 @SpringBootApplication
 public class ApiG46BdApplication {
 
