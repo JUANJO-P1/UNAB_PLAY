00f152945e19d55a1f83da35a58f357f897f7b00
JUANJO-P1 <jujogalo714@gmail.com>
2022-11-30 21:25:35 -0500

    comit prueba

--- ApiG46BD/src/main/java/com/example/ApiG46BD/ApiG46BdApplication.java
+++ ApiG46BD/src/main/java/com/example/ApiG46BD/ApiG46BdApplication.java
@@ -2,7 +2,7 @@ package com.example.ApiG46BD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//prueba
+//prueba juan
 @SpringBootApplication
 public class ApiG46BdApplication {
 
